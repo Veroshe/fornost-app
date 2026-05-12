716e84e5838451407687bcad4ab20318770936bf
Weronika Kalinowska <wkalinowska406@gmail.com>
2026-05-12 12:12:23 +0200

    Close gg

--- src/pages/GG.page.tsx
+++ src/pages/GG.page.tsx
@@ -110,9 +110,9 @@ const linksData = [
   {
     icon: IconUsers,
     label: 'Formularz zgłoszeniowy i wakaty',
+    description: 'Zgłoszenia zakończone',
     color: 'etherealBlue',
-    available: true,
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSeAmBHaFOUIBHWrgmsDBuDtBgwfS-JEM0lYsS6bJsv9ROfCXg/viewform',
+    available: false,
   },
 ];
 
